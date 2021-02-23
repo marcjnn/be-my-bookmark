dd2e1eb225c98471e17dddedb7ce63bf0a43adb1
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-23 19:40:31 +0100

    import font awesome for react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import "./Header.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
+  const userIcon = <FontAwesomeIcon icon={faUserCircle} />;
   return (
     <header className="header">
       <h1 className="hidden">be my bookmark</h1>
       <h2 className="header__logo">bm</h2>
-      <nav className="header__menu">
-        <i className="fas fa-user-circle"></i>
-      </nav>
+      <nav className="header__menu">{userIcon}</nav>
     </header>
   );
 };
