1d0657f8a922a77625812a1f73751f22aa098fa5
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-23 20:06:45 +0100

    update style in sub-menu in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,95 +1,105 @@
 import "./Main.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 const Main = () => {
+  const plusIcon = <FontAwesomeIcon icon={faPlus} />;
   return (
-    <main class="personalMain">
-      <nav class="subMenu column">
-        <ul class="navBar">
-          <li>1</li>
-          <li>2</li>
-          <li>
-            <i class="fas fa-plus"></i>
-          </li>
+    <main className="main">
+      <nav className="main__menu">
+        <ul className="menu">
+          <li className="menu__item">1</li>
+          <li className="menu__item">2</li>
+          <li className="menu__item">{plusIcon}</li>
         </ul>
       </nav>
-      <section class="boards column">
-        <div class="newBoard">
-          <h3 class="boardName">front-end</h3>
-          <ul class="boardSection">
+      <section className="boards column">
+        <div className="newBoard">
+          <h3 className="boardName">front-end</h3>
+          <ul className="boardSection">
             <li>
-              <h3 class="boardSectionName">html</h3>
+              <h3 className="boardSectionName">html</h3>
             </li>
             <li>
-              <ul class="bookmarks">
+              <ul className="bookmarks">
                 <li>
-                  <h4 class="bmTitle">html art 1</h4>
-                  <p class="bmDescription">description art 1 or abstract</p>
+                  <h4 className="bmTitle">html art 1</h4>
+                  <p className="bmDescription">description art 1 or abstract</p>
                 </li>
               </ul>
             </li>
             <li>
-              <ul class="bookmarks">
+              <ul className="bookmarks">
                 <li>
-                  <h4 class="bmTitle">html art 2</h4>
-                  <p class="bmDescription">description art 2 or abstract</p>
+                  <h4 className="bmTitle">html art 2</h4>
+                  <p className="bmDescription">description art 2 or abstract</p>
                 </li>
               </ul>
             </li>
           </ul>
-          <ul class="boardSection">
+          <ul className="boardSection">
             <li>
-              <h3 class="boardSectionName">css</h3>
+              <h3 className="boardSectionName">css</h3>
             </li>
             <li>
-              <ul class="bookmarks">
+              <ul className="bookmarks">
                 <li>
-                  <h4 class="bmTitle">css art 1</h4>
-                  <p class="bmDescription">description art 1 or abstract</p>
+                  <h4 className="bmTitle">css art 1</h4>
+                  <p className="bmDescription">description art 1 or abstract</p>
                 </li>
               </ul>
             </li>
             <li>
-              <ul class="bookmarks">
+              <ul className="bookmarks">
                 <li>
-                  <h4 class="bmTitle">css art 2</h4>
-                  <p class="bmDescription">description art 2 or abstract</p>
+                  <h4 className="bmTitle">css art 2</h4>
+                  <p className="bmDescription">description art 2 or abstract</p>
                 </li>
               </ul>
             </li>
             <li>
-              <ul class="bookmarks">
+              <ul className="bookmarks">
                 <li>
-                  <h4 class="bmTitle">css art 3</h4>
-                  <p class="bmDescription">description art 3 or abstract</p>
+                  <h4 className="bmTitle">css art 3</h4>
+                  <p className="bmDescription">description art 3 or abstract</p>
                 </li>
               </ul>
             </li>
           </ul>
         </div>
-        <div class="newBoard">
-          <h3 class="boardName">childs psychology</h3>
-          <ul class="bookmarks">
+        <div className="newBoard">
+          <h3 className="boardName">childs psychology</h3>
+          <ul className="bookmarks">
             <li>
-              <h4 class="bmTitle">psycho art 1</h4>
-              <p class="bmDescription">psycho art 1 description or abstract</p>
+              <h4 className="bmTitle">psycho art 1</h4>
+              <p className="bmDescription">
+                psycho art 1 description or abstract
+              </p>
             </li>
           </ul>
-          <ul class="bookmarks">
+          <ul className="bookmarks">
             <li>
-              <h4 class="bmTitle">psycho art 2</h4>
-              <p class="bmDescription">psycho art 2 description or abstract</p>
+              <h4 className="bmTitle">psycho art 2</h4>
+              <p className="bmDescription">
+                psycho art 2 description or abstract
+              </p>
             </li>
           </ul>
-          <ul class="bookmarks">
+          <ul className="bookmarks">
             <li>
-              <h4 class="bmTitle">psycho art 3</h4>
-              <p class="bmDescription">psycho art 3 description or abstract</p>
+              <h4 className="bmTitle">psycho art 3</h4>
+              <p className="bmDescription">
+                psycho art 3 description or abstract
+              </p>
             </li>
           </ul>
-          <ul class="bookmarks">
+          <ul className="bookmarks">
             <li>
-              <h4 class="bmTitle">psycho art 4</h4>
-              <p class="bmDescription">psycho art 4 description or abstract</p>
+              <h4 className="bmTitle">psycho art 4</h4>
+              <p className="bmDescription">
+                psycho art 4 description or abstract
+              </p>
             </li>
           </ul>
         </div>
