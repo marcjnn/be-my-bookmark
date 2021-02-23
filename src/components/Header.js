adea8abc2c67f0e104a946e66403d68119f34640
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-23 19:28:16 +0100

    update style in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,12 @@ import "./Header.scss";
 
 const Header = () => {
   return (
-    <header class="headMobile">
-      <div class="headerWrapper column">
-        <h2 class="logo">bm</h2>
-        <nav>
-          <ul class="navBar">
-            <li>
-              <i class="fas fa-user-circle"></i>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <header className="header">
+      <h1 className="hidden">be my bookmark</h1>
+      <h2 className="header__logo">bm</h2>
+      <nav className="header__menu">
+        <i className="fas fa-user-circle"></i>
+      </nav>
     </header>
   );
 };
