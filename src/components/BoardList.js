fb74222dec41b8b517e0f0b84d297ffa33ca3319
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-27 12:56:22 +0100

    clean code

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,11 +1,10 @@
 import Board from "./Board";
 
 const BoardList = (props) => {
-  // console.log("props form app ", props);
-  const boardstoprint = props.boards.map((board, index) => {
+  const boards = props.boards.map((board, index) => {
     return <Board key={index} board={board} />;
   });
-  return <>{boardstoprint}</>;
+  return <>{boards}</>;
 };
 
 export default BoardList;
